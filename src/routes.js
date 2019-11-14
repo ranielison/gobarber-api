12f877ebdd0c15b4bb4e2728a207135296515ce4
Ranielison <ranielisonsoares2@gmail.com>
2019-11-13 22:35:57 -0300

    criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ msg: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Ranielison',
+    email: 'ranielisonsoares2@gmail.com',
+    password_hash: 'aksjdkasdjkasd',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
