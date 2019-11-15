8065ef20750f3487323f788119c0c197ec0002d3
Ranielison <ranielisonsoares2@gmail.com>
2019-11-15 12:13:14 -0300

    salvando id do avatar do usuario no banco de dados

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,7 +18,5 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
