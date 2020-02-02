ae908a1957134f6f3e56dec4192bf3da1586bedf
Ranielison <ranielisonsoares2@gmail.com>
2020-02-02 11:15:57 -0300

    Fazendo cancelamento de agendamento

--- src/routes.js
+++ src/routes.js
@@ -32,5 +32,6 @@ routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 export default routes;
