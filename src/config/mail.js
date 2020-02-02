454985d96c70ddc6b88a8dbc78dfa327ee7ad888
Ranielison <ranielisonsoares2@gmail.com>
2020-02-02 13:26:24 -0300

    Configurando nodmailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'f290fecaacafb2',
+    pass: 'e79e8a39754874',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
