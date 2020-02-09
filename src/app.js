a4e80f009046fa8ee9890bff3099148bd3d2b8dc
Ranielison <ranielisonsoares2@gmail.com>
2020-02-08 22:40:56 -0300

    Configurando cors e retornando alguns dados

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
